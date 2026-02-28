83f68fe095ec45b19e0d63724f9b5b7d7fc7ec3e
codekrolik2 <john.amirov.83@gmail.com>
2026-02-28 02:53:55 -0900

    initial stub commit

---
+++ PoplavokUI/src/main/java/com/poplavok/AppLauncher.java
@@ -0,0 +1,13 @@
+package com.poplavok;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AppLauncher {
+    final static Logger LOGGER = LoggerFactory.getLogger(AppLauncher.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting Poplavok");
+        App.main(args);
+    }
+}
