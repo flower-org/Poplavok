bfbb9a963e2230a09a70b4040d815d21783f547e
codekrolik2 <john.amirov.83@gmail.com>
2026-08-25 21:37:05 -0800

    Version updated to 1.1.1

--- PoplavokUI/src/main/java/com/poplavok/forms/MainForm.java
+++ PoplavokUI/src/main/java/com/poplavok/forms/MainForm.java
@@ -52,7 +52,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "Poplavok v 1.1.0", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "Poplavok v 1.1.1", ButtonType.OK);
         alert.showAndWait();
     }
 
